a48e523b0f90e704a1f677c66e67cf796b8eb675
Kiran Subedi <subedikiran41@gmail.com>
2026-02-13 01:14:20 +0545

    code refactor

--- internal/v1/v1_msg_handler.go
+++ internal/v1/v1_msg_handler.go
@@ -26,36 +26,3 @@ func (m *Version1MsgHandler) Version1MessageHandler(v1MsgPayload interface{}) er
 	}
 	return nil
 }
-
-//
-//func (m *MessageHandler) doAuth() error {
-//	log.Println("Server requires authentication.")
-//	stdin := bufio.NewReader(os.Stdin)
-//
-//	log.Println("Enter username and password to login in the server.")
-//	log.Println("Username: ")
-//	fmt.Print(" ")
-//	username, err := stdin.ReadString('\n')
-//	if err != nil {
-//		return fmt.Errorf("failed to read username: %w", err)
-//	}
-//
-//	log.Println("Password: ")
-//	fmt.Print(" ")
-//	password, err := stdin.ReadString('\n')
-//	if err != nil {
-//		return fmt.Errorf("failed to read password: %w", err)
-//	}
-//
-//	//return c.send(core.Message{
-//	//	MessageType:     core.MsgAuth,
-//	//	IsAuthenticated: false,
-//	//	Payload: core.AuthMessagePayload{
-//	//		Payload: core.AuthPayload{
-//	//			Username: strings.TrimSpace(username),
-//	//			Password: strings.TrimSpace(password),
-//	//		},
-//	//		Type: core.MsgAuthRequest,
-//	//	},
-//	//})
-//}
